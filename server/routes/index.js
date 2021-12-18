0f98426b6163d5cd529cf8c9d462b4bae4de63c9
unknown <golden.star0228@gmail.com>
2021-12-17 23:53:13 -0500

    updated router

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,9 @@ const Controllers = require('../controllers')
 
 const router = new Router()
 
-router.all('/*', Controllers.Main.authenticate)
+router.post('/*', Controllers.Main.authenticate)
+router.put('/*', Controllers.Main.authenticate)
+router.delete('/*', Controllers.Main.authenticate)
 
 router.post('/buy-ticket', Controllers.Main.buyTicket)
 
